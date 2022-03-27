e0c454fbbfab621412c936ed8c3788c774e051aa
Bhavana <bhavananerlakota@gmail.com>
2022-03-27 15:42:02 +0530

    added program for Line Comparision computation

---
+++ Line Comparision computation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Line_Comparision_computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation");
+
+            Console.WriteLine("Please enter x1 and y1 coordinates");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            int y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter x2 and y2 coordinates");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            int y2=Convert.ToInt32(Console.ReadLine());
+            length.UC1(x1,y1,x2,y2);
+
+            Console.WriteLine("Welcome to Line Equality program");
+
+            Console.WriteLine("Please enter l1 and m1 coordinates");
+            int l1 = Convert.ToInt32(Console.ReadLine());
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter l2 and m2 coordinates");
+            int l2 = Convert.ToInt32(Console.ReadLine());
+            int m2 = Convert.ToInt32(Console.ReadLine());
+            length.UC2(x1, y1, x2, y2);
+
+            Console.WriteLine("To Compare Both Line by CompareTo method");
+            Console.WriteLine("Please Enter a1 and b1 coordinates");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a2 and b2 coordinates");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            length.UC3(l1, m1, l2, m2);
+
+
+
+
+        }
+    }
+}
---
+++ Line Comparision computation/length.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision_computation
+{
+    internal class length
+    {
+        public static void UC1(int x1, int x2, int y1, int y2)
+        {
+            double temp1, temp2, result;
+            temp1 = Math.Pow((x2 - x1), 2);
+            temp2 = Math.Pow((y2 - y1), 2);
+            result = Math.Sqrt(temp1 + temp2);
+
+            Console.WriteLine("Length between Coordinates is" + result);
+
+        }
+        public static void UC2(int l1, int m1, int l2, int m2)
+        {
+            int len1, len2;
+            if (l1 == l2)
+            {
+                Console.WriteLine(m1.Equals(m2));
+            }
+            else
+            {
+                len1 = m1 - l1;
+                len2 = m2 - l2;
+                Console.WriteLine(len1.Equals(len2));
+            }
+        }
+        public static void UC3(int a1, int b1, int a2, int b2)
+        {
+            int le1, le2;
+            if (a1 == a2)
+            {
+                int temp = b1.CompareTo(b2);
+                if (temp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (temp >= 0)
+                {
+                    Console.WriteLine("Second line is less than first line");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is greater than First line");
+                }
+            }
+            else
+            {
+                le1 = b1 - a1;
+                le2 = b2 - a2;
+                int temp = le1.CompareTo(le2);
+                if (temp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (temp >= 0)
+                {
+                    Console.WriteLine("Second line is less than first line");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is greater than First line");
+                }
+            }
+        }
+    }
+}
